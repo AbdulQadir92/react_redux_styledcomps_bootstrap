5be3ba14d208ebae27d7b266c1a6cbd3823371e1
AbdulQadir92 <a_qadir_jamali@hotmail.com>
2022-12-15 19:22:59 +0500

    Resolved a minor issue

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import { NotFoundStyled } from "../styles/components/NotFound.styled"
+import { NotFoundStyled } from "../styles/pages/NotFound.styled"
 
 
 const NotFound = () => {
