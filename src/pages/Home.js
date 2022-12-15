c2d419ba820b66dc008e847164e797c7f7267405
AbdulQadir92 <a_qadir_jamali@hotmail.com>
2022-12-15 18:48:46 +0500

    Added Modal and removed Carousel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,17 @@
 import { HomeStyled } from '../styles/pages/Home.styled'
-import Carousel from '../components/home/Carousel'
+import Modal from '../components/Modal'
+
 
 const Home = () => {
     return (
         <HomeStyled>
-            <Carousel />
             <h1>Home</h1>
+
+            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Launch demo modal
+            </button>
+
+            <Modal />
         </HomeStyled>
     )
 }
